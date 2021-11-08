bb0e0cb075bd675a39bea0f081cd8c4702388902
NaRHa0713 <e1b19104@oit.ac.jp>
2021-11-08 22:10:35 +0900

    ログイン機能の実装

---
+++ src/main/java/oit/is/z0571/kaizi/janken/controller/Lec02Controller.java
@@ -0,0 +1,22 @@
+package oit.is.z0571.kaizi.janken.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Lec02Controller
+ *
+ * クラスの前に@Controllerをつけていると，HTTPリクエスト（GET/POSTなど）があったときに，このクラスが呼び出される
+ */
+@Controller
+public class Lec02Controller {
+
+  @GetMapping("/lec02")
+  public String lec02() {
+    return "lec02.html";
+  }
+}
